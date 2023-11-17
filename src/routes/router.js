a9ef6eb2afeeb8c72a22c3e52f588fc64328ae54
KemonoBAT4 <kemono.bat.4@gmail.com>
2023-11-17 18:51:35 +0100

    added scripts, modified 404 page

--- src/routes/router.js
+++ src/routes/router.js
@@ -2,18 +2,16 @@ const express = require("express");
 const pageRouter = express.Router();
 
 pageRouter.get("/", async(req, res) => {
-    //res.render("home");
-    //document.location = "/home"
     res.render("home");
 })
 
 pageRouter.get("/home", async(req, res) => {
     res.render("home");
+
 })
 
 pageRouter.get("*", async(req, res) => {
     res.render("404");
 })
-
 
 module.exports = pageRouter;
