146915487e088393e404a6261d34789d42044f6b
KemonoBAT4 <kemono.bat.4@gmail.com>
2023-12-02 20:56:47 +0100

    added database logic

---
+++ src/database/tables/games/routes.js
@@ -0,0 +1,50 @@
+const router = require("express").Router();
+
+// Middleware Imports
+const isAuthenticatedMiddleware = require("/middlewares/IsAuthenticatedMiddleware");
+const SchemaValidationMiddleware = require("/middlewares/SchemaValidationMiddleware");
+const CheckPermissionMiddleware = require("/middlewares/CheckPermissionMiddleware");
+
+// Controller Imports
+const GameController = require("./controllers/GameController");
+
+// JSON Schema Imports for payload verification
+const updateUserPayload = require("./schemas/updateUserPayload");
+const changeRolePayload = require("./schemas/changeRolePayload");
+
+const { roles } = require("/home/config");
+
+router.get("/", [isAuthenticatedMiddleware.check], GameController.getUser);
+
+router.patch(
+  "/",
+  [
+    isAuthenticatedMiddleware.check,
+    SchemaValidationMiddleware.verify(updateUserPayload),
+  ],
+  UserController.updateUser
+);
+
+router.get(
+  "/all",
+  [isAuthenticatedMiddleware.check, CheckPermissionMiddleware.has(roles.ADMIN)],
+  UserController.getAllUsers
+);
+
+/*router.patch(
+  "/change-role/:userId",
+  [
+    isAuthenticatedMiddleware.check,
+    CheckPermissionMiddleware.has(roles.ADMIN),
+    SchemaValidationMiddleware.verify(changeRolePayload),
+  ],
+  UserController.changeRole
+);*/
+
+router.delete(
+  "/:id",
+  [isAuthenticatedMiddleware.check, CheckPermissionMiddleware.has(roles.ADMIN)],
+  UserController.deleteUser
+);
+
+module.exports = router;
--- start.js
+++ start.js
@@ -1,26 +1,69 @@
+
 const express = require("express");
 const app = express();
-const port = 5000;
+const { port } = require("./config");
+const cors = require("cors");
+const morgan = require("morgan");
+const { Sequelize } = require("sequelize");
 
-//Static files
+// Express Routes Import
+const pageRouter = require("./src/routes/router");
+const userRoutes = require("./src/database/controllers/users/routes");
+const gameRoutes = require("./src/database/controllers/games/routes");
+//const apiRouter = require("./src/routes/") //TODO: create the folder and finish this line
+
+// Sequelize model imports
+const UserModel = require("./src/database/models/User");
+const NewsModel = require("./src/database/models/News");
+
+app.use(morgan("tiny"));
+app.use(cors());
+
+// Middlewares
+app.use(express.json()); //TODO: see if this line is usefull
+
+// Static files
 app.use(express.static("public"));
 
-//TODO: check if this lines is correct
 //app.use("/",express.static("./node_modules/bootstrap/dist/"));
+
+// Client static files
 app.use("/bootstrap", express.static("./node_modules/bootstrap/dist/css"));
 app.use("/css", express.static(__dirname + "public/css"));
 app.use("/js", express.static(__dirname + "public/js"));
 app.use("/imgs", express.static(__dirname + "public/imgs"));
 app.use("/json", express.static(__dirname + "public/json"));
 
+// Server static files
+app.use("/models", express.static(__dirname + "src/database/models"));
+app.use("/storage", express.static(__dirname + "src/database/storage"));
+app.use("/middlewares", express.static(__dirname + "src/database/middlewares"));
+app.use("/controllers", express.static(__dirname + "src/database/controllers"))
+//app.use("/home", express.static(__dirname));
+console.log(__dirname)
+
+// Ejs engine
 app.set("views", "./src/views/pages");
 app.set("view engine", "ejs");
 
 
-const pageRouter = require("./src/routes/router");
-//Routes
-app.use("/", pageRouter);
-app.use("/home", pageRouter);
+// Sequelize
+const sequelize = new Sequelize({
+    dialect: "sqlite",
+    storage: "./src/database/storage/data.db" //Path to the file that will store the SQLite DB
+});
 
-//Listen on port 5000
-app.listen(process.env.PORT || port,  () => console.log(`Listening on port ${port}`));
+UserModel.initialise(sequelize);
+//NewsModel.initialise(sequelize);
+
+
+sequelize.sync().then(() => {
+    console.log("Sequelize Initialised!");
+
+    // Attacching Routes to the app
+    app.use("/", pageRouter);
+    //app.use("/api", apiRouter); //TODO: add finish to create all the files and stuff for this and then remove the comment
+    app.listen(process.env.PORT || port,  () => console.log(`Listening on port ${port}`));
+}).catch((error) => {
+    console.error("Sequelize Initialisation threw an error: ", error)
+});
