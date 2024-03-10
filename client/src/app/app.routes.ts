2eae2862163600b13e94c42693bc27b423a69ce4
KemonoBAT4 <kemono.bat.4@gmail.com>
2024-03-10 19:15:24 +0100

    modified routes

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from '../components/home/home.component';
+import { LoginComponent } from '../components/login/login.component';
 
 export const routes: Routes = [
     {
@@ -8,8 +9,17 @@ export const routes: Routes = [
         pathMatch: 'full'
     },
     {
+        // HOME PAGE
         path: 'home',
         component: HomeComponent
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        // 404
+        path: '**',
+        redirectTo: "home"
     }
-
 ];
